f18fe3e63f7a5a8633ce7e1c0a5eb99accee9df4
Leonardo Da Pozzo <l.dapozzo398@gmail.com>
2025-03-09 13:20:52 +0100

    manually install muscle during pre-install

--- scripts/pre_install.py
+++ scripts/pre_install.py
@@ -8,6 +8,69 @@ def main():
     wizard_root = sys.argv[1]
     env_name = sys.argv[2]
 
+    # install muscle manually
+    print("Installing MUSCLE...")
+    muscle_dir = os.path.join(wizard_root, "ext", "muscle")
+    Path(muscle_dir).mkdir(exist_ok=True)
+    subprocess.run(
+        [
+            "conda",
+            "run",
+            "--no-capture-output",
+            "-n",
+            f"{env_name}",
+            "wget",
+            "https://www.drive5.com/muscle/muscle_src_3.8.1551.tar.gz",
+        ],
+        cwd=muscle_dir,
+    )
+
+    subprocess.run(
+        [
+            "conda",
+            "run",
+            "--no-capture-output",
+            "-n",
+            f"{env_name}",
+            "tar",
+            "xzvf",
+            "muscle_src_3.8.1551.tar.gz",
+        ],
+        cwd=muscle_dir,
+    )
+
+    subprocess.run(
+        [
+            "conda",
+            "run",
+            "--no-capture-output",
+            "-n",
+            f"{env_name}",
+            "make",
+        ],
+        cwd=muscle_dir,
+    )
+
+    conda_base_path = str(
+            subprocess.check_output("conda info --base", shell=True), "utf-8"
+        ).strip()
+    conda_prefix = os.path.join(conda_base_path, "envs", env_name)
+    subprocess.run(
+        [
+            "conda",
+            "run",
+            "--no-capture-output",
+            "-n",
+            f"{env_name}",
+            "cp",
+            "muscle",
+            os.path.join(conda_prefix, "bin"),
+        ],
+        cwd=muscle_dir,
+    )
+
+
+
     #TODO: only install if missing
     print("Installing ANARCI...")
     anarci_dir = os.path.join(wizard_root, "ext", "ANARCI")
